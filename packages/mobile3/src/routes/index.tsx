11428ce79326642b5123c621431ece258941873e
Leonardo Lima <leonardo1230987@gmail.com>
2021-12-05 23:24:33 -0400

    Corrigindo Actions

--- packages/mobile3/src/routes/index.tsx
+++ packages/mobile3/src/routes/index.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import React from 'react'
+import { View, ActivityIndicator } from 'react-native'
 
-import {useAuth} from '../contexts/auth';
+import { useAuth } from '../contexts/auth'
 
-import AdminRoutes from '../routes/admin.routes';
-import UserRoutes from '../routes/user.routes';
-import SignRoutes from '../routes/sign.routes';
+import AdminRoutes from './admin.routes'
+import UserRoutes from './user.routes'
+import SignRoutes from './sign.routes'
 
 const Routes: React.FC = () => {
-  const {signed, user, loading} = useAuth();
+  const { signed, user, loading } = useAuth()
 
   if (loading) {
     return (
       // eslint-disable-next-line react-native/no-inline-styles
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" color="#f15a57" />
       </View>
-    );
+    )
   }
 
   if (!signed) {
-    return <SignRoutes />;
+    return <SignRoutes />
   }
-  return user?.level === 2 ? <AdminRoutes /> : <UserRoutes />;
-};
+  return user?.level === 2 ? <AdminRoutes /> : <UserRoutes />
+}
 
-export default Routes;
+export default Routes
